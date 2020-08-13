bbbad4c4904bbdfa904e6000e4a33e10ebb1797b
DuGlaser <duglaser0618@gmail.com>
2020-08-13 18:38:18 +0900

    :sparkles: Add the TrackingObject class

---
+++ Assets/Scripts/TrackingObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class TrackingObject : MonoBehaviour
+{
+
+    [SerializeField]
+    private XRReferenceObjectLibrary xRReferenceObjectLibrary;
+
+    [SerializeField]
+    private GameObject prefabOnTrack;
+
+    void Start()
+    {
+      ARTrackedObjectManager trackedObjectManager = gameObject.AddComponent<ARTrackedObjectManager>();
+      trackedObjectManager.referenceLibrary = xRReferenceObjectLibrary;
+      trackedObjectManager.trackedObjectPrefab = prefabOnTrack;
+
+      trackedObjectManager.enabled = true;
+      /* trackedObjectManager.trackedObjectsChanged += OnTrackedObjectChanged; */
+    }
+
+    /* private void OnTrackedObjectChanged(ARTrackedObjectsChangedEventArgs eventArgs) */
+    /* { */
+    /* } */
+}
