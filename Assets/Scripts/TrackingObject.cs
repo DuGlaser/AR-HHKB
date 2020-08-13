b8964ee6e4b32352aa73ffda4eae42ebed396986
DuGlaser <duglaser0618@gmail.com>
2020-08-14 01:47:02 +0900

    :construction: [WIP] Add the process to display navigatorUI

--- Assets/Scripts/TrackingObject.cs
+++ Assets/Scripts/TrackingObject.cs
@@ -6,21 +6,35 @@ using UnityEngine.XR.ARSubsystems;
 
 public class TrackingObject : MonoBehaviour
 {
-
     [SerializeField]
     private XRReferenceObjectLibrary xRReferenceObjectLibrary;
 
     [SerializeField]
     private GameObject prefabOnTrack;
 
+    [SerializeField]
+    private GameObject navigatorUI;
+
+    private ARTrackedObjectManager trackedObjectManager;
+
     void Start()
     {
-      ARTrackedObjectManager trackedObjectManager = gameObject.AddComponent<ARTrackedObjectManager>();
-      trackedObjectManager.referenceLibrary = xRReferenceObjectLibrary;
-      trackedObjectManager.trackedObjectPrefab = prefabOnTrack;
+        navigatorUI.SetActive(true);
 
-      trackedObjectManager.enabled = true;
-      /* trackedObjectManager.trackedObjectsChanged += OnTrackedObjectChanged; */
+        ARTrackedObjectManager trackedObjectManager = gameObject.AddComponent<ARTrackedObjectManager>();
+        trackedObjectManager.referenceLibrary = xRReferenceObjectLibrary;
+        trackedObjectManager.trackedObjectPrefab = prefabOnTrack;
+
+        trackedObjectManager.enabled = true;
+        /* trackedObjectManager.trackedObjectsChanged += OnTrackedObjectChanged; */
+    }
+
+    void Update()
+    {
+        /* if (trackedObjectManager.isActiveAndEnabled) */
+        /* { */
+        /*     navigatorUI.SetActive(false); */
+        /* } */
     }
 
     /* private void OnTrackedObjectChanged(ARTrackedObjectsChangedEventArgs eventArgs) */
